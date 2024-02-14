523dd77ed5a353ec7eb5a19f254d1ad0457b102b
faithful <faithfulaborisade123@gmail.com>
2024-02-14 18:07:10 +0000

    form, list and first servelt completed. jsp file created

--- CartyMeatsProject/src/main/java/com/linkedin/servelt/CatalogServlet.java
+++ CartyMeatsProject/src/main/java/com/linkedin/servelt/CatalogServlet.java
@@ -1,8 +1,12 @@
 package com.linkedin.servelt;
 
 import java.io.IOException;
+ 
+ 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,32 +14,54 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class CatalogServlet
  */
-@WebServlet("/CatalogServlet")
+@WebServlet(urlPatterns = "/CatalogServlet")
 public class CatalogServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CatalogServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	public CatalogServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.getWriter().append(request.getParameter("name"));
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String name = request.getParameter("name");
+		String manufacturer = request.getParameter("manufacturer");
+		String sku = request.getParameter("sku");
+
+		response.setHeader("someHeader", "someHeaderValue");
+		response.addCookie(new Cookie("someCookie", "someCookieValue"));
+
+		Catalog.addItem(new CatalogItem(name, manufacturer, sku));
+		
+		request.setAttribute("items", Catalog.getItems());
+		RequestDispatcher dispatcher = request.getRequestDispatcher("list.jsp");
+		dispatcher.forward(request, response);
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
---
+++ CartyMeatsProject/src/main/java/com/linkedin/servelt/ParameterLoggingFilter.java
@@ -0,0 +1,55 @@
+package com.linkedin.servelt;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+
+/**
+ * Servlet Filter implementation class ParameterLoggingFilter
+ */
+@WebFilter(urlPatterns= "/*", asyncSupported = true)
+public class ParameterLoggingFilter extends HttpFilter implements Filter {
+       
+    /**
+     * @see HttpFilter#HttpFilter()
+     */
+    public ParameterLoggingFilter() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		request.getParameterMap().entrySet().stream().forEach(entry ->{
+			System.out.println(String.format("%s:%s",entry.getKey(), entry.getValue()[0]));
+		});
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
